573ac011b79864d3300fd18299815c6fc5c8518b
Eric Pfahl <eric.pfahl@gmail.com>
2024-02-18 19:29:56 -0500

    add test fixture

--- test/cem_sample_test.exs
+++ test/cem_sample_test.exs
@@ -4,36 +4,30 @@ defmodule CEMSampleTest do
 
   alias CEM.Sample
 
-  test "generate a sample and score with mode max" do
-    {instances, score} =
-      Sample.generate_elite_sample_and_score(
-        0,
-        fn _ -> 1 end,
-        fn _ -> 1 end,
-        %{
-          mode: :max,
-          n_sample: 10,
-          n_elite: 3
-        }
-      )
+  setup_all do
+    {:ok,
+     generate_fn: fn mode ->
+       Sample.generate_elite_sample_and_score(
+         0,
+         fn _ -> 1 end,
+         fn _ -> 1 end,
+         %{
+           mode: mode,
+           n_sample: 10,
+           n_elite: 3
+         }
+       )
+     end}
+  end
 
+  test "generate a sample and score with mode max", %{generate_fn: generate_fn} do
+    {instances, score} = generate_fn.(:max)
     assert length(instances) == 3
     assert score == 1
   end
 
-  test "generate a sample and score with mode min" do
-    {instances, score} =
-      Sample.generate_elite_sample_and_score(
-        0,
-        fn _ -> 1 end,
-        fn _ -> 1 end,
-        %{
-          mode: :min,
-          n_sample: 10,
-          n_elite: 3
-        }
-      )
-
+  test "generate a sample and score with mode min", %{generate_fn: generate_fn} do
+    {instances, score} = generate_fn.(:min)
     assert length(instances) == 3
     assert score == 1
   end
