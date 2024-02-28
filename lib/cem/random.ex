cc3fd21c193e9a8e4a530c94069ceda5583ee8e3
Eric Pfahl <eric.pfahl@gmail.com>
2024-02-28 08:38:18 -0500

    add random variate generators

---
+++ lib/cem/random.ex
@@ -0,0 +1,54 @@
+defmodule CEM.Random do
+  @moduledoc """
+  Convenience functions for generating random variates.
+  """
+
+  @doc """
+  Generate a uniform variate from 0 to 1.
+  """
+  @spec uniform() :: float()
+  def uniform(), do: :rand.uniform()
+
+  @doc """
+  Generate a uniform variate from `min_value` to `max_value`.
+  """
+  @spec uniform(number(), number()) :: float()
+  def uniform(min_value, max_value), do: min_value + (max_value - min_value) * :rand.uniform()
+
+  @doc """
+  Generate a normal variate with mean `mean` and standard deviation `std`.
+  """
+  @spec normal(number(), number()) :: float()
+  def normal(mean, std), do: mean + std * :rand.normal()
+
+  @doc """
+  Generate a bernoulli variate (0 or 1) with probability `p` of being 1.
+  """
+  @spec bernoulli(number()) :: 0 | 1
+  def bernoulli(p), do: if(:rand.uniform() <= p, do: 1, else: 0)
+
+  @doc """
+  Generate a random value from a list of `{value, weight}` tuples.
+
+  The probability of selecting a value is proportional to its weight.
+  The weights do not need to be normalized.
+
+  An exception is raised if the list is empty.
+  """
+  @spec choice([{any(), number()}]) :: any()
+  def choice([]) do
+    raise ArgumentError,
+      message: "choice/1 must be called with a non-empty list of value-weight tuples"
+  end
+
+  def choice([{_, w} | _] = vws) when is_number(w) do
+    u = uniform() * Enum.reduce(vws, 0, fn {_, w}, sum -> sum + w end)
+    choice(vws, 0, u)
+  end
+
+  @spec choice([{any(), number()}], number(), number()) :: any()
+  defp choice([{v, w} | rest], cumsum, u) do
+    cumsum_next = cumsum + w
+    if u <= cumsum_next, do: v, else: choice(rest, cumsum_next, u)
+  end
+end
